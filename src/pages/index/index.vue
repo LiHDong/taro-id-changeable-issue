73edbc59bd6b86c3dfa2620987aca18d30891438
lihuadong <1651139630@qq.com>
2021-03-11 09:54:39 +0800

    复现taro生产环境编译打包后已赋值的id变化的问题

---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <navigator url="/pages/detail/index">详情页</navigator>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './index.css'
+
+export default {
+  setup () {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+}
+</script>
